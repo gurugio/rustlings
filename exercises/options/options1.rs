079ed1d1c78a9acbf601c67e5ccc07c6af73c7de
Gioh Kim <gurugio@gmail.com>
2022-11-22 22:45:58 +0900

    improve options1

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -33,7 +33,7 @@ mod tests {
     #[test]
     fn raw_value() {
         // TODO: Fix this test. How do you get at the value contained in the Option?
-        let icecreams = maybe_icecream(12).unwrap();
+        let icecreams = maybe_icecream(12).unwrap_or(0);
         assert_eq!(icecreams, 5);
     }
 }
