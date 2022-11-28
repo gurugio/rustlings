70ecbd0ceea9707e76c00ab8a789cd87f5db33b1
Gioh Kim <gurugio@gmail.com>
2022-11-29 00:21:21 +0900

    box1

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,11 +16,9 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +31,12 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let a = List::Cons(1, Box::new(List::Nil));
+    List::Cons(2, Box::new(a))
 }
 
 #[cfg(test)]
@@ -51,6 +50,7 @@ mod tests {
 
     #[test]
     fn test_create_non_empty_list() {
+        let mut x = create_non_empty_list();
         assert_ne!(create_empty_list(), create_non_empty_list())
     }
 }
